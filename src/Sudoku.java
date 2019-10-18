c65cc2d9de1e91583ceab94131b8fc8213bb0939
karfsen <martin.krendzelak@akademiasovy.sk>
2019-10-18 08:30:01 +0200

    FIxed Final version

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -24,8 +24,8 @@ public class Sudoku {
     }
 
     public boolean boxCheck(int col,int row,int num){
-        for(int i=row%3;i<row%3+3;i++){
-            for(int j=col%3;j<col%3;j++){
+        for(int i=(row/3)*3;i<(row/3)*3+3;i++){
+            for(int j=(col/3)*3;j<(col/3)*3+3;j++){
                 if(gameplan[i][j]==num){
                     return true;
                 }
@@ -38,10 +38,10 @@ public class Sudoku {
         for(int row=0;row<9;row++){
             for (int col=0;col<9;col++){
                 if(gameplan[row][col]==0){
-                    for (int i=1;i<9;i++){
-                        System.out.println("skušam "+i+" na pozicii ["+row+","+col+"]");
-                        if (!(rowCheck(row,i)||colCheck(col,i)||boxCheck(row,col,i))){
-                            System.out.println("davam "+i+" na poziciu ["+row+","+col+"]");
+                    for (int i=1;i<10;i++){
+                        //System.out.println("skušam "+i+" na pozicii ["+row+","+col+"]");
+                        if (!(rowCheck(row,i)||colCheck(col,i)||boxCheck(col,row,i))){
+                            //System.out.println("davam "+i+" na poziciu ["+row+","+col+"]");
                             gameplan[row][col]=i;
                             writeMatrix();
                             if(solverino()){
